37f4e8c5d71c9841e3996b8a78b0d3b1e586f102
Truett <truett.davis@gmail.com>
2023-02-15 20:56:00 -0500

    added several files and made edits to the manifest and background javascript files

---
+++ background.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // isolate and assign the title tag to a variable
+  let title = document.querySelector("title");
+  // assign the inner text of the title tag to a variable
+  let titleText = title.innerText;
+
+  // IDEA: connect to chatGPT API and use the titleText as a search query 
+
+  // fetch request to chatGPT API passing in the necessary information
+
+});
